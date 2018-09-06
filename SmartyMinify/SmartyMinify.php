44263e428659a2a15109c7c9589ffcf8a6637890
yr4ik <yr4ik_07@ukr.net>
2018-09-07 00:37:32 +0300

    + version 1.1

---
+++ SmartyMinify/SmartyMinify.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+*	@ name: SmartyMinify
+*	@ version: 1.1
+*	@ description: Расширения для работы с js/css 
+*	@ author: Polevik Yurii
+*	@ author_url: http://vk.com/polevik_yuriy
+*/
+
+
+
+class SmartyMinify extends vqInstaller {
+	
+	
+
+	public function __construct(){
+		
+		$_errors = array();
+		
+		$vqmod_control = $this->mods->get('vqmod_control');
+		
+		if(!$vqmod_control || version_compare($vqmod_control->version, '2.8', '<'))
+			$_errors[] = 'Need Simpla vQmod version 2.8 or higher';
+
+		if(!empty($_errors))
+			throw new Exception(implode('<br />', $_errors), 503);
+		
+		$this->form->addElement(new Element_HTML('<legend>'.$this->mod->name . ' ' . $this->mod->version . '</legend>'));
+	}
+	
+
+	public function install(){
+
+		if(!$this->is_confirmed()){
+			
+			$this->form->addElement(new Element_HTML('<p>'. $this->mod->description .'</p>
+			<p>Установить?</p>'));
+			
+			$this->form->addElement(new Element_Hidden('confirmed', 'yes'));
+			$this->form->addElement(new Element_Button('Да', 'submit'));
+			
+			$this->form->addElement(new Element_Button('Отмена', 'button', array(
+				'class' => 'btn-default',
+				'onclick' => "window.location='/'"
+			)));
+
+
+		}else{
+			
+			
+			$this->installer->exec('manifest.xml', 'install');
+
+			$counters = $this->installer->get_counter();
+
+			$result_log = "<div class=\"text-left\"><div>Установлено {$counters->copied} файлов</div></div><br>";
+			
+			
+			foreach($this->installer->get_results('errors') as $error)
+				$result_log .= "<div class=\"alert alert-danger\">{$error}</div>";
+
+			$result_log .= "<div class=\"alert alert-success\">Модуль установлен</div>";
+			
+			$this->form->addElement(new Element_HTML($result_log));
+
+			$this->form->addElement(new Element_Button('Перейти на сайт', 'button', array(
+				'class' => 'btn-default',
+				'onclick' => "window.location='/'"
+			)));
+			
+			$this->mod->status = 'installed';
+		}
+
+		return $this->form->render(true);
+	}
+	
+	
+	public function uninstall(){
+
+
+		if(!$this->is_confirmed()){
+			
+			$this->form->addElement(new Element_HTML('<p>Вы подтверждаете удаление?</p>'));
+
+			$this->form->addElement(new Element_Hidden('confirmed', 'yes'));
+			$this->form->addElement(new Element_Button('Удалить', 'submit'));
+			
+			$this->form->addElement(new Element_Button('Отмена', 'button', array(
+				'class' => 'btn-default',
+				'onclick' => "window.location='/'"
+			)));
+
+		}else{
+			
+
+			$this->installer->exec('manifest.xml', 'uninstall');
+
+
+			$counters = $this->installer->get_counter();
+			$result_log = "<div class=\"text-left\"><div>Удалено {$counters->deleted_file} файлов</div></div><br>";
+									
+			foreach($this->installer->get_results('errors') as $error)
+				$result_log .= "<div class=\"alert alert-danger\">{$error}</div>";
+
+			$result_log .= "<div class=\"alert alert-success\">Модуль удален</div>";
+			
+			$this->form->addElement(new Element_HTML($result_log));
+			
+			$this->form->addElement(new Element_Button('Перейти на сайт', 'button', array(
+				'class' => 'btn-default',
+				'onclick' => "window.location='/'"
+			)));
+			
+			$this->mod->status = 'uninstalled';
+			
+		}
+
+
+		return $this->form->render(true);
+	}
+	
+
+
+	private function is_confirmed(){
+		return $this->request->post('confirmed', 'boolean');
+	}	
+	
+	 
+}
+
---
+++ SmartyMinify/files/api/Stylesheet.php
@@ -0,0 +1,301 @@
+<?php
+
+/**
+ * Stylesheet
+ *
+ * @copyright	2017 Polevik yurii
+ * @author		 Polevik yurii
+ *
+ */
+ 
+
+class Stylesheet extends Simpla
+{
+	
+	protected $events = array();
+	
+	protected $gzip_level = null;
+	
+	protected $cache_dir = 'css/';
+	
+	protected $min_filesize = 256;
+	
+	protected $order_num = 0;
+
+	/*
+	* Регистрация css фал(а|ов)
+	* @param $id
+	* @param $files
+	* @param integer $priority
+	*/
+	public function add_files($id, $files, $priority=10)
+	{
+		$event = $this->get_event($id);
+		
+		foreach((array) $files as $path)
+		{
+			$file = trim($path, '/ ');
+			$path = VQMod::modCheck($this->config->root_dir . $file);
+			if(is_file($path))
+				$event->data[$path] = (object) array('type'=>'file', 'time'=>filemtime($path), 'original'=>$file, 'event'=>$event->id);
+		}
+
+		if(!$event->data)
+			return false;
+		
+		$event->priority = intval($priority);
+		
+		return $this->events[$event->id] = $event;
+	}
+
+	/*
+	* Регистрация произвольного css кода
+	* @param $id
+	* @param string $code
+	* @param integer $priority
+	*/
+	public function add_code($id, $code, $priority=10)
+	{
+		$event = $this->get_event($id);
+		
+		if(!$code = trim($code))
+			return false;
+		
+		$event->data[$code] = (object) array('type'=>'code', 'time'=>0, 'event'=>$event->id);
+		$event->priority = intval($priority);
+		
+		return $this->events[$event->id] = $event;
+	}
+	
+	/*
+	* Отмена регистрации css фал(а|ов) или кода
+	* @param $id
+	*/
+	public function unplug($id)
+	{
+		unset($this->events[$id]);
+	}
+	
+	
+	/*
+	* Вывод css фал(а|ов) или кода
+	* @param $event
+	*/
+	public function render($event_id=null, $minify=null, $combine=true)
+	{
+		
+		if(is_null($minify))
+			$minify = $this->config->minify_css;
+		
+		// тут может использоваться сжатие
+		// проверим поддерживается ли оно браузером
+		if(is_null($this->gzip_level))
+		{
+			$this->gzip_level = 0;
+			
+			// Создаем паку если не существует
+			$this->cache_dir =  $this->config->minify_cache_dir . $this->cache_dir;
+			if(!is_dir($this->config->root_dir . $this->cache_dir))
+				mkdir($this->config->root_dir . $this->cache_dir, 0755, true);
+				
+			
+			if($this->config->minify_gzip_level > 0 && isset($_SERVER['HTTP_ACCEPT_ENCODING']))
+			{
+				if (stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false)
+				{
+					if(function_exists('ob_gzhandler') && !ini_get('zlib.output_compression'))
+					{
+						$this->gzip_level = $this->config->minify_gzip_level;
+					}
+				}
+			}
+		}
+
+		// Если задан айди ресурса отдадим только его
+		if(!is_null($event_id))
+		{
+			if(isset($this->events[$event_id]))
+			{
+				$events_data = $this->events[$event_id]->data;
+				// Очищаем от повторного рендеринга
+				$this->unplug($event_id);
+			}
+		}
+		else
+		{
+			uasort($this->events, array($this, 'sort_priority_callback'));
+			
+			$events_data = array();
+			foreach($this->events as $ev)
+				$events_data = array_merge($events_data, $ev->data);
+			
+			// Очищаем от повторного рендеринга
+			$this->events = array();
+		}
+
+		$result = '';
+		
+		//Если нет ничего для вывода
+		if(empty($events_data))
+			return $result;
+
+		
+		// Все выключено отдаем оригиналы 
+		if(!$combine && !$minify)
+		{
+			foreach($events_data as $css=>$data)
+			{
+				if($e->type == 'code')
+					 $result .= $this->render_tag($css);
+				 else
+					 $result .= $this->render_tag(false, $data->original);
+			}
+		}
+		else // Что то включено
+		{
+			
+			// Если не пакуем данные в 1 файл
+			if(!$combine)
+			{
+				foreach($events_data as $css => $e)
+				{
+					if($e->type == 'code')
+						$prefix = $e->event;
+					else
+						$prefix = pathinfo($e->original, PATHINFO_FILENAME);
+					
+					$result .= $this->proteced(array($css => $e), $prefix, $minify);
+				}
+			}
+			else // Пакуем в все в 1 файл
+			{
+				
+				$prefix = 'pack';
+				if(count($events_data)==1)
+				{
+					$e = reset($events_data);
+					if($e->type == 'code')
+						$prefix = $e->event;
+					else
+						$prefix = pathinfo($e->original, PATHINFO_FILENAME);
+				}
+				$result = $this->proteced($events_data, $prefix, $minify);
+			}
+		}
+		
+	
+		return $result;
+	}
+
+	
+	
+	protected function proteced($data, $prefix, $minify)
+	{
+		
+		if($minify && substr($prefix, -4)!=='.min')
+			$prefix .= '.min';
+
+		list($cacheFile, $cachePath) = $this->get_cacheFile($data, $prefix);
+
+		// Нет основного кеш-файла 
+		if(!is_file($cachePath))
+		{
+			$content = $this->minify(array_keys($data), $cachePath, $minify);
+			if($this->gzip_level && !$content)
+				$cacheFile = $cacheFile.'.gz'.$this->gzip_level;
+		}
+		else
+		{
+			$content = false;
+			if(filesize($cachePath) < $this->min_filesize)
+				$content = file_get_contents($cachePath);
+			elseif($this->gzip_level)
+				$cacheFile = $cacheFile.'.gz'.$this->gzip_level;
+		}
+
+		return $this->render_tag($content, $cacheFile);
+	}
+
+	
+	
+	protected function get_event($event_id)
+	{
+		if(isset($this->events[$event_id]))
+			return $this->events[$event_id];
+		
+		$event = new stdClass();
+		$event->id = $event_id;			
+		$event->data = array();			
+		$event->order = $this->order_num++;			
+		return $event;
+	}
+			
+
+	
+	protected function render_tag($content, $css_file)
+	{
+		if($content)
+			return '<style type="text/css">' . $content . '</style>';
+		else
+			return '<link href="' . $css_file . '" rel="stylesheet"/>';
+	}
+	
+	
+	protected function minify($data, $cache, $minify)
+	{
+		// Используем сжатие
+		require_once $this->config->root_dir . '/resize/MatthiasMullie/autoload.php';
+		if($minify)
+		{
+			$minifier = new MatthiasMullie\Minify\CSS($data);
+			$content = $minifier->minify($cache);
+		}
+		else // Просто клеим
+		{
+			$minifier = new MatthiasMullie\Minify\CSSPacker($data);
+			$content = $minifier->minify($cache);
+		}
+		
+		// Если контент меньше min_filesize то отдаем его в html
+		if(strlen($content) > $this->min_filesize)
+			$content = false;
+
+		// Возвращаем контент или false (если отдаем файлами)
+		return $content;
+	}
+
+	/*
+	* Формируем название кеш-файла исходя из параметров
+	*/
+	protected function get_cacheFile($data, $prefix)
+	{
+		$key = $this->hash(var_export($data, 1));
+
+		$cacheFile = $this->cache_dir . $key . '_' . $prefix . '.css';
+		return array($cacheFile, $this->config->root_dir . $cacheFile);
+	}
+			
+			
+	/*
+	* Хеширование строки
+	*/
+	protected function hash($str)
+	{
+		$hash = crc32($str);
+		return ($hash>=0 ? $hash:sprintf('%u', $hash));
+	}
+		
+		
+	/* 
+	* Сортируем массив по приоритету 
+	*/
+	public function sort_priority_callback($a, $b)
+	{
+		if ($a->priority == $b->priority)
+			return ($a->order < $b->order) ? -1 : 1;
+
+		return ($a->priority < $b->priority) ? 1 : -1;
+	}
+	
+
+}
